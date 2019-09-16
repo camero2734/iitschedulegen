3bc8ecfa19b8a43bb1846f42b397707003325a81
Cameron Haley <camero2734@outlook.com>
2019-09-16 12:31:09 -0500

    jquery

--- genSchedule.js
+++ genSchedule.js
@@ -1,20 +1,4 @@
-async function loadJSON(path) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                return JSON.parse(xhr.responseText);
-            } else {
-                console.log("error fetching json");
-                return {};
-            }
-        }
-    };
-    xhr.open("GET", path, true);
-    xhr.send();
-}
-
-!(async function() {
-    let classes = loadJSON("classes.json");
+(async function() {
+    let classes = await $.getJSON("classes.json");
     console.log(classes);
 })();
